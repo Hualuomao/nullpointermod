8093be6408021477a3fcf1336f750c37a5676c6a
Hualuomao <3849439838@qq.com>
2026-08-28 18:53:32 +0800

    Fix ModItems registration to pass Item.Properties to constructors

--- src/main/java/com/example/nullpointermod/item/ModItems.java
+++ src/main/java/com/example/nullpointermod/item/ModItems.java
@@ -11,7 +11,7 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, NullPointerMod.MOD_ID);
 
     public static final RegistryObject<Item> JAVA_ITEM = ITEMS.register("java_item",
-            () -> new JavaItem(new Item.Properties()));
+            () -> new JavaItem(new Item.Properties().stacksTo(1)));
 
     public static final RegistryObject<Item> NULL_POINTER_ITEM = ITEMS.register("java_null_pointer_exception",
             () -> new NullPointerItem(new Item.Properties()));
